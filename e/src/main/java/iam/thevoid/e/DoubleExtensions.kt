352072268ae95acf86a50a2619a01224bc045ddb
iam <alesetar@gmail.com>
2019-05-24 00:11:22 +0300

    precision for format double and float

--- e/src/main/java/iam/thevoid/e/DoubleExtensions.kt
+++ e/src/main/java/iam/thevoid/e/DoubleExtensions.kt
@@ -1,6 +1,6 @@
 package iam.thevoid.e
 
-fun Double.format(): String =
+fun Double.format(precision : Int? = null): String =
     if (toLong().toDouble() == this)
         toLong().toString()
-    else toString()
+    else precision?.let { ("%." + (if (it < 0) 0 else it ) + "f").format(this) } ?: toString()
--- e/src/main/java/iam/thevoid/e/FloatExtensions.kt
+++ e/src/main/java/iam/thevoid/e/FloatExtensions.kt
@@ -1,6 +1,6 @@
 package iam.thevoid.e
 
-fun Float.format(): String =
+fun Float.format(precision: Int? = null): String =
     if (toLong().toFloat() == this)
         toLong().toString()
-    else toString()
+    else precision?.let { ("%." + (if (it < 0) 0 else it) + "f").format(this) } ?: toString()
