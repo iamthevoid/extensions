22ff8590149ccdf82984d11d62951fd33c9c67fd
iam <alesetar@gmail.com>
2019-07-30 10:35:53 +0300

    optional update, libs update (0.5.2)

--- util/src/main/java/iam/thevoid/util/Optional.kt
+++ util/src/main/java/iam/thevoid/util/Optional.kt
@@ -1,18 +1,21 @@
 package iam.thevoid.util
 
-class Optional<T> private constructor(val elem: T?) {
+class Optional<T> private constructor(val item: T?) {
 
     @get:JvmName("isPresent")
     val present
-        get() = elem != null
+        get() = item != null
 
     val notNull
-        get() = elem!!
+        get() = item!!
 
-    fun orElse(item : T) = elem ?: item
+    fun orElse(item : T) = this.item ?: item
 
     companion object {
+        @JvmStatic
         fun <T> of(elem: T?): Optional<T> = Optional(elem)
+
+        @JvmStatic
         fun <T> empty(): Optional<T> = Optional(null)
     }
 }
