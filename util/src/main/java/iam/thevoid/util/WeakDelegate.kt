271b8bfa49579aa44e8e6cb48b8a2ce5b3537ff6
iam <alesetar@gmail.com>
2019-08-03 16:05:36 +0300

    WeakDelegate (0.5.3)

---
+++ util/src/main/java/iam/thevoid/util/WeakDelegate.kt
@@ -0,0 +1,15 @@
+package iam.thevoid.util
+
+import java.lang.ref.WeakReference
+import kotlin.reflect.KProperty
+
+class WeakDelegate<T> {
+
+    private var reference: WeakReference<T> = WeakReference(null)
+
+    operator fun setValue(host: Any, property: KProperty<*>, newReference: T?) {
+        reference = WeakReference<T>(newReference)
+    }
+
+    operator fun getValue(host: Any, property: KProperty<*>) = reference.get()
+}
