2b2ff7050a6a61d62f454d1e217106e09b0084d0
iam <alesetar@gmail.com>
2019-11-06 21:15:00 +0300

    rx util + string ext (0.5.13) (fixes)

--- rxe/src/main/java/iam/thevoid/rxe/CompletableUtil.kt
+++ rxe/src/main/java/iam/thevoid/rxe/CompletableUtil.kt
@@ -1,9 +1,8 @@
-@file:JvmName("MaybeUtil")
+@file:JvmName("CompleteableUtil")
 
 package iam.thevoid.rxe
 
 import io.reactivex.Completable
-import io.reactivex.Maybe
 import io.reactivex.Single
 
 fun <T> Completable.toSingle(item : () -> T) = andThen(Single.just(item()))
